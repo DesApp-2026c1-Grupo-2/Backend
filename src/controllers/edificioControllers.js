8357151d9805d18a5a23d5b5a24f79101f21ffad
leonel-pisani <leonel.pisani@estudiantes.unahur.edu.ar>
2026-05-12 11:02:45 -0300

    actualizando los controllers

--- src/controllers/edificioControllers.js
+++ src/controllers/edificioControllers.js
@@ -4,20 +4,32 @@ const Edificio = require('../models/edificio.model');
 // C: Crear un nuevo edificio
 const crearEdificio = async (req, res) => {
   try {
-    const { nombre, direccion, cantidadAulas } = req.body;
-    const edificio = new Edificio({ nombre, direccion, cantidadAulas });
+    const { nombre, direccion } = req.body;
+
+    const edificio = new Edificio({
+      nombre,
+      direccion,
+    });
+
     await edificio.save();
+
     res.status(201).json(edificio);
+
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
 
+
 // R: Obtener todos los edificios
 const obtenerEdificios = async (req, res) => {
   try {
-    const edificios = await Edificio.find();
+
+    const edificios = await Edificio.find()
+      .populate("cantidadLaboratorios");
+
     res.status(200).json(edificios);
+
   } catch (error) {
     res.status(500).json({ message: error.message });
   }

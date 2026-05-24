901e4d97a639b34597ac7c8b1f8f6e4653d67e7e
Juanmabritez <juanmanuelbritez23@gmail.com>
2026-05-24 15:24:07 -0300

    Agrego PUT y DELETE Logico de edificios

--- src/routes/edificioRoutes.js
+++ src/routes/edificioRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const router = Router()
-const { crearEdificio, obtenerEdificios } = require('../controllers/edificioControllers');
+const { crearEdificio, obtenerEdificios, obtenerEdificioPorId, actualizarEdificio,eliminarEdificioLogico} = require('../controllers/edificioControllers');
 
 
 // C: Crear un nuevo edificio
@@ -9,4 +9,10 @@ router.post('/', crearEdificio);
 // R: Obtener todos los edificios
 router.get('/', obtenerEdificios);
 
+// R: Obtener un edificio por ID
+router.get('/:id', obtenerEdificioPorId);
+// U: Actualizar un edificio
+router.put('/:id', actualizarEdificio);
+// D: Eliminar un edificio (lógico)
+router.delete('/:id', eliminarEdificioLogico);
 module.exports = router;

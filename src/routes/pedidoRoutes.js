1ad180133494d6e29076bec193df113aebf7ca1a
leonel-pisani <leonel.pisani@estudiantes.unahur.edu.ar>
2026-05-27 19:26:01 -0300

    actualizar las rutas

--- src/routes/pedidoRoutes.js
+++ src/routes/pedidoRoutes.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const pedidoControllers = require("../controllers/pedidoControllers");
+
 const validarPedido = require("../middlewares/validatePedidos");
+const { validarJWT, validarRol } = require("../middlewares/validateJWT");
 
 // GET: Obtener todos los pedidos o uno por ID
-router.get("/", pedidoControllers.getPedidos);
-router.get("/:id", pedidoControllers.getPedidoById);
+router.get("/", validarJWT, pedidoControllers.getPedidos);
+router.get("/:id", validarJWT, pedidoControllers.getPedidoById);
 
 // POST / PUT: Crear y actualizar pedidos (Pasan por el middleware de validación real)
-router.post("/", validarPedido, pedidoControllers.createPedido);
-router.put("/:id", validarPedido, pedidoControllers.updatePedido);
+// TODOS PUEDEN CREAR
+router.post("/", validarJWT, validarPedido, pedidoControllers.createPedido);
+// SOLO PERSONAL Y ADMIN PUEDEN ACTUALIZAR
+router.put("/:id", validarJWT, validarRol("PERSONAL","ADMIN"), validarPedido, pedidoControllers.updatePedido);
 
 // PATCH: Modificaciones de estado y procesos de negocio
-router.patch("/:id/estado", pedidoControllers.updateEstado);
-router.patch("/:id/aprobar", pedidoControllers.aprobarPedido); // Ruta dedicada para aprobar y descontar stock
-router.patch("/:id/finalizar", pedidoControllers.finalizarPedido); // Ruta para cerrar el pedido y liberar equipos
+// SOLO PERSONAL Y ADMIN PUEDEN CAMBIAR ESTADO, APROBAR O FINALIZAR
+router.patch("/:id/estado", validarJWT, validarRol("PERSONAL","ADMIN"), pedidoControllers.updateEstado);
+router.patch("/:id/aprobar", validarJWT, validarRol("PERSONAL","ADMIN"), pedidoControllers.aprobarPedido); // Ruta dedicada para aprobar y descontar stock
+router.patch("/:id/finalizar", validarJWT, validarRol("PERSONAL","ADMIN"), pedidoControllers.finalizarPedido); // Ruta para cerrar el pedido y liberar equipos
 
-router.delete("/:id", pedidoControllers.borrarPedido);
+// DELETE: Eliminar un pedido (SOLO ADMIN PUEDE ELIMINAR)
+// A CHEQUEAR SI SOLO EL ADMIN
+router.delete("/:id", validarJWT, validarRol("ADMIN"), pedidoControllers.borrarPedido);
 
 module.exports = router;

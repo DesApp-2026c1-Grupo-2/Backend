bf4e71e5944f8a92fb1056f419305918fae311a0
leonel-pisani <leonel.pisani@estudiantes.unahur.edu.ar>
2026-05-28 19:04:09 -0300

    ahora si solo muestras los pedidos del docente que los realizo

--- src/controllers/pedidoControllers.js
+++ src/controllers/pedidoControllers.js
@@ -4,16 +4,15 @@ const Equipo = require("../models/equipo.model");
 const Item = require("../models/item.model");
 
 const getPedidos = async (req, res) => {
-
   try {
 
-    const { uid, rol } = req.usuario;
+    const { id, rol } = req.usuario;
 
     let filtro = {};
 
     // DOCENTE → solo sus pedidos
     if (rol === "DOCENTE") {
-      filtro.docente = uid;
+      filtro.docente = id;
     }
 
     const pedidos = await Pedido.find(filtro)
@@ -36,12 +35,9 @@ const getPedidos = async (req, res) => {
 };
 
 const getPedidoById = async (req, res) => {
-
   try {
-
+    const { id: userId, rol } = req.usuario;
     const { id } = req.params;
-
-    const { uid, rol } = req.usuario;
 
     const pedido = await Pedido.findById(id)
       .populate("docente", "nombre apellido email")
@@ -51,31 +47,21 @@ const getPedidoById = async (req, res) => {
         select: "nombre tipo codigo esFijo estado",
       });
 
-    // No existe
     if (!pedido) {
-      return res.status(404).json({
-        error: "Pedido no encontrado"
-      });
+      return res.status(404).json({ error: "Pedido no encontrado" });
     }
 
-    // Si es docente → verificar dueño
     if (
       rol === "DOCENTE" &&
-      pedido.docente._id.toString() !== uid
+      pedido.docente._id.toString() !== userId
     ) {
-
-      return res.status(403).json({
-        error: "No autorizado"
-      });
+      return res.status(403).json({ error: "No autorizado" });
     }
 
     res.json(pedido);
 
   } catch (error) {
-
-    res.status(500).json({
-      error: error.message
-    });
+    res.status(500).json({ error: error.message });
   }
 };
 

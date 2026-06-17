854e8fdaf06295bdce9595cd85c649d61a809f04
franiel <66392105+franiiel@users.noreply.github.com>
2026-06-17 13:25:47 -0300

    add CRUD de actidad y funcionalidad de sugerencias en actividad

--- src/models/actividad.model.js
+++ src/models/actividad.model.js
@@ -3,6 +3,12 @@ import mongoose from "mongoose";
 const actividadSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   fecha: { type: Date, required: true },
+  tipo: {
+    type: String,
+    enum: ['quimica', 'biologia', 'teorica'],
+    required: true,
+    index: true
+  },
   estado: { 
     type: String, 
     enum: ['planificada', 'en_proceso', 'finalizada'], 

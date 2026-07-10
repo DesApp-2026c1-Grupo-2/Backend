9186c5dd0f012d50b40267acc0c6497f8134ef70
Juanmabritez <juanmanuelbritez23@gmail.com>
2026-07-10 14:53:01 -0300

    fix sintaxis

--- src/middlewares/validateDescartes.js
+++ src/middlewares/validateDescartes.js
@@ -1,26 +1,19 @@
 import { registrarDescarteSchema } from "../schemas/descarteSchema.js";
 
 const validarRegistrarDescarte = (req, res, next) => {
-  try {
-    const { error, value } = registrarDescarteSchema.validate(req.body, {
-      abortEarly: false,
-      stripUnknown: true,
-    });
+  const { error, value } = registrarDescarteSchema.validate(req.body, {
+    abortEarly: false,
+    stripUnknown: true,
+  });
 
-    if (error) {
-      return res.status(400).json({
-        error: error.details.map((detalle) => detalle.message),
-      });
-    }
-
-    req.body = value;
-
-    next();
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message,
+  if (error) {
+    return res.status(400).json({
+      error: error.details.map((detalle) => detalle.message),
     });
   }
+
+  req.body = value;
+  next();
 };
 
 export default validarRegistrarDescarte;
